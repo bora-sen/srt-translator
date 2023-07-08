a48c9998a352762195321c6a64ff7b3df5d6df69
bora-sen <bahattinborasen@gmail.com>
2023-07-08 12:47:17 +0300

    +added translation logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,34 @@
-import { Box, Button, Card, CardBody, CardFooter, CardHeader, Container, FormLabel, Heading, Image, InputGroup, Link, Select, Text } from "@chakra-ui/react"
+import { Avatar, Box, Button, Card, CardBody, CardFooter, CardHeader, Container, FormLabel, Heading, InputGroup, Link, Select, Text } from "@chakra-ui/react"
 import languages from "./languages.json"
 import { useEffect, useState } from "react"
+import * as srtparser from "srtparsejs"
 
 function App() {
+  async function translateText(baseLang, targetLang, text) {
+    const req = await fetch(`https://translate.googleapis.com/translate_a/single?client=gtx&sl=${baseLang}&tl=${targetLang}&dt=t&q=${encodeURI(text)}`)
+    const resp = await req.json()
+    const translated = resp[0][0][0]
+    return translated
+  }
+
+  async function translateQuoteObject(baseLang, targetLang, quote) {
+    const translatedText = await translateText(baseLang, targetLang, quote.text)
+    quote.text = translatedText
+    quote.id = parseInt(quote.id)
+    return quote
+  }
+
+  async function translateSrt(srt, baseLang, targetLang) {
+    const parsed = srtparser.parse(srt)
+    const translatedArr = []
+    for (let i = 0; i < parsed.length; i++) {
+      let translatedQuote = await translateQuoteObject(baseLang, targetLang, parsed[i])
+      translatedArr.push(await translatedQuote)
+    }
+    const renderedSrt = srtparser.toSrt(translatedArr)
+    return renderedSrt
+  }
+
   const [srtText, setSrtText] = useState("")
   const [baseLang, setBaseLang] = useState("")
   const [targetLang, setTargetLang] = useState("")
@@ -20,22 +46,30 @@ function App() {
     console.log("Base Language is Set To => ", targetLang)
   }, [targetLang])
 
-  function handleClick(e) {
+  async function handleClick(e) {
     e.preventDefault()
     const srtInput = document.getElementById("srt_file_input")
+    console.log(srtInput.value)
     const srtFile = srtInput.files[0]
     const reader = new FileReader()
     reader.readAsText(srtFile, "UTF-8")
-    reader.onload = function (evt) {
-      console.log(evt.target.result)
+    reader.onload = async function (evt) {
       setSrtText(evt.target.result)
+      let translatedSrt = await translateSrt(evt.target.result, baseLang, targetLang)
+      console.log(await translatedSrt)
+      const link = document.createElement("a")
+      const file = new Blob(await [translatedSrt], { type: "text/plain" })
+      link.href = URL.createObjectURL(await file)
+      link.download = `${srtInput.nodeValue}.srt`
+      link.click()
+      URL.revokeObjectURL(await link.href)
     }
   }
   return (
     <Container maxW="full" h="100dvh" backgroundColor="red.200" display="flex" justifyContent="center" alignItems="center">
       <Card maxW="50rem">
         <CardHeader>
-          <Heading>Test</Heading>
+          <Heading marginBottom={2}>Test</Heading>
           <Text>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi, pariatur? Blanditiis tempora autem magnam reiciendis voluptas molestiae harum,
             ipsam aspernatur consectetur sint ad illum illo nulla impedit sed natus inventore aperiam necessitatibus ratione cumque quas cupiditate magni
@@ -43,8 +77,7 @@ function App() {
           </Text>
         </CardHeader>
         <CardBody>
-          <Image src="https://loremflickr.com/1280/720" />
-          <Box display="flex" marginY={3}>
+          <Box display="flex" marginBottom={4}>
             <Box>
               <FormLabel>Select Base Language</FormLabel>
               <Select onChange={(e) => setBaseLang(getCodeFromLanguage(e.target.value))} name="base_language_select" placeholder="Base Language">
@@ -63,12 +96,14 @@ function App() {
             </Box>
           </Box>
           <InputGroup display="grid" gap={3}>
-            <input type="file" name="srt_file_input" id="srt_file_input" />
+            <input accept=".srt" type="file" name="srt_file_input" id="srt_file_input" />
             <Button onClick={(e) => handleClick(e)}>Translate Srt File</Button>
           </InputGroup>
+          <Box id="download_link_container"></Box>
         </CardBody>
         <CardFooter>
-          <Link href="https://github.com/bora-sen" fontWeight="bold" colorScheme="teal">
+          <Link display="flex" justifyContent="center" alignItems="center" href="https://github.com/bora-sen" fontWeight="bold" colorScheme="teal">
+            <Avatar width={12} height={12} marginRight={1} name="Bora Şen" src="https://github.com/bora-sen.png" />
             @github
           </Link>
         </CardFooter>
